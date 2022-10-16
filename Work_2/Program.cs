6115b30b95487dc7df212613e1d0758acc3952ef
DenisIG333 <ignatovdnc@inbox.ru>
2022-10-16 14:33:20 +0300

    Домашнее задание

---
+++ Work_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number > number2)
+{
+    Console.Write($"Число {number} больше числа {number2}");
+}
+
+if(number2 > number)
+{
+    Console.Write($"Число {number2} больше числа {number}");
+}
+
+if(number2 == number)
+{
+    Console.Write($"Число {number2} равно числу {number}");
+}
---
+++ Work_4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+int number3 = int.Parse(Console.ReadLine());
+
+int[] array = {number1, number2, number3};
+
+int index = 0;
+int max = array[index];
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[index] > max)
+    {
+        max = array[index];
+    }
+    index++;
+
+}
+
+Console.Write($"Максимальное число - {max}");
